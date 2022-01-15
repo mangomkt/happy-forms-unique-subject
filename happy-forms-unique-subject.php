091083b8aa2c9c80aad7d7d40cc97bfe5cc70e34
mangomkt <cg@mangomkt.com>
2022-01-15 09:36:40 -0600

    First push and basic setup

---
+++ happy-forms-unique-subject.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: Happy Forms Unique Subject Email Notification
+Plugin URI: https://github.com/mangomkt/happy-forms-unique-subject
+Description: Manage Custom Built Modules
+Version: 0.01
+Author: Curtis Grant
+
+*/
+
+add_filter( 'happyforms_email_alert', function( $email_message ) {
+    $reply_to = $email_message->get_reply_to();
+
+    if ( is_array( $reply_to ) && ! empty( $reply_to ) ) {
+        $email_message->set_from( $reply_to[0] );
+    }
+
+    $subject = $email_message->get_subject();
+    $form_id = $email_message->message['form_id'];
+    $form = happyforms_get_form_controller()->get( $form_id );
+    $parts = wp_list_pluck( $form['parts'], 'id', 'label' );
+    $message_parts = $email_message->message['parts'];
+
+    $name_label = 'First Name';
+    $name_value = '';
+
+    if ( isset( $parts[$name_label] ) ) {
+        $name_value = $message_parts[$parts[$name_label]];
+        $subject .= ' — ' . $name_value;
+
+        $email_message->set_from_name( $name_value );
+    }
+
+    $email_message->set_subject( $subject );
+
+    return $email_message;
+} );
+
+?>
